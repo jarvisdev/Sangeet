c812911df2e9b87a021544ec11c23e5698198b31
Milan Bavishi <33129756+jarvisdev@users.noreply.github.com>
2018-01-18 21:54:58 +0530

    removed API key

--- signact.php
+++ signact.php
@@ -15,7 +15,7 @@
 	          echo '<h2>Please check the the captcha form.</h2>';
 	          exit;
 	        }
-        $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LddwDIUAAAAAMXqGq_dWyeqDMLsXojv5VCzyBmE&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']);
+        $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=your_api_key&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']);
         if($response.success==false)
         {
          echo '<h2>You are spammer ! Get the @$%K out</h2>';
